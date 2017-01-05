8dd77a4d113e574c782f16f1fb9aa1ae50d3af3b
AkhilaAchanta <akhila.achanta@gmail.com>
2017-01-06 01:43:18 +0530

    Admin working

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { render } from 'react-dom'
+
+import { Router, Route, hashHistory } from 'react-router'
+
+import App from './modules/App'
+
+import Login from './modules/Login'
+
+import Repos from './modules/Repos'
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/repos" component={Repos}/>
+    <Route path="/login" component={Login}/>
+  </Router>
+), document.getElementById('app'))
---
+++ src/modules/Login.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './css/animate.css';
+import './css/style.css';
+
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div className="container">
+    		<div className="top">
+    			<h1 id="title" className="animated fadeInDown"><span id="logo">Admin <span>Login</span></span></h1>
+    		</div>
+    		<div className="login-box animated fadeInUp">
+    			<div className="box-header">
+    				<h2 >Log in</h2>
+    			</div>
+    			<label forName="username">Username</label>
+    			<br/>
+    			<input type="text" id="username" />
+    			<br/>
+    			<label forName="password">Password</label>
+    			<br/>
+    			<input type="password" id="password" />
+    			<br/>
+    			<button type="submit">Sign In</button>
+    			<br/>
+    			<a href="Register"><p className="small">Forgot your password?</p></a>
+    		</div>
+    	</div>
+    );
+  }
+}
+class Register extends Component {
+
+  render() {
+    return (
+      <div className="container">
+    		<div className="top">
+    			<h1 id="title" className="animated fadeInDown"><span id="logo">Admin <span>Login</span></span></h1>
+    		</div>
+    		<div className="login-box animated fadeInUp">
+    			<div className="box-header">
+    				<h2 >Log in</h2>
+    			</div>
+    			<label forName="username">Username</label>
+    			<br/>
+    			<input type="text" id="username" />
+    			<br/>
+    			<label forName="password">Password</label>
+    			<br/>
+    			<input type="password" id="password" />
+    			<br/>
+          <label forName="password">Confirm Password</label>
+          <br/>
+          <input type="password" id="password1" />
+          <br/>
+    			<button type="submit">Sign In</button>
+    			<br/>
+    			<a href=""><p className="small">Forgot your password?</p></a>
+    		</div>
+    	</div>
+    );
+  }
+}
+
+export default Login;
